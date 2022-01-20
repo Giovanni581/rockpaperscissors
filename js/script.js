ffc3318d6319c02137eb51f80c7b3168d95d39c2
Glitch (secretive-abrasive-slime) <none>
2022-01-20 00:25:35 +0000

    🤲🆕 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let randomNumber = 0;
-randomNumber = Math.random() *3;
 $(".play").click(function() {
+  randomNumber = Math.ceil(Math.random() *3);
      let choice = $(".input").val();
   $(".userChoice").text(`${choice}`);
+   $(".computerChoice").text(`${randomNumber}`);
 });
